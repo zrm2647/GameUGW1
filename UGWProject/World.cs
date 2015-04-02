9161407c68f1c92884e138542edb5a9b6722a50d
Ro <rc7604@g.rit.edu>
2015-04-01 21:59:59 -0400

    things
    s

---
+++ UGWProject/World.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UGWProject
+{
+    class World
+    {
+        //This class will handle both the Afterlife and the Physical world. 
+        //A bool will differentiate which object are active/interactable during 
+        //each stage
+
+        //in here will be arrays/lists of enemies, blocks, and interactables
+
+
+    }
+}
